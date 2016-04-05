e2fd588f3f5b66e877197a27a69476a874d11b1d
MoerchenOppi <Rambox2@web.de>
2016-04-05 10:09:25 +0200

    MineBot - Erstellung
    - Als Lerngegner für den CHEBot wurde der MineBot erstellt.
    - Dieses nimmt nur Minen ein und heilt sich.
    - Kein Kampf

---
+++ vindinium-client-1.0.0/src/main/java/com/brianstempin/vindiniumclient/bot/simple/MineBot.java
@@ -0,0 +1,206 @@
+package com.brianstempin.vindiniumclient.bot.simple;
+
+import com.brianstempin.vindiniumclient.bot.BotMove;
+import com.brianstempin.vindiniumclient.bot.BotUtils;
+import com.brianstempin.vindiniumclient.dto.GameState;
+
+import java.util.*;
+import java.util.logging.Logger;
+
+public class MineBot implements SimpleBot {
+
+    private Logger logger;
+
+    public MineBot() {
+        logger = Logger.getLogger("minebot");
+    }
+
+    private static List<Vertex> doDijkstra(GameState.Board board, GameState.Hero hero) {
+        List<Vertex> vertexes = new LinkedList<Vertex>();
+        Vertex me = null;
+
+        // Graph ohne Kanten erstellen
+        for (int row = 0; row < board.getSize(); row++) {
+            for (int col = 0; col < board.getSize(); col++) {
+                Vertex v = new Vertex();
+                GameState.Position pos = new GameState.Position(row, col);
+                int tileStart = row * board.getSize() * 2 + (col * 2);
+                v.setTileType(board.getTiles().substring(tileStart, tileStart + 1 + 1));
+                v.setPosition(pos);
+                vertexes.add(v);
+            }
+        }
+
+        // Kanten hinzufügen
+        for (int i = 0; i < board.getSize() * board.getSize(); i++) {
+            int row = i % (board.getSize());
+            int col = i / board.getSize();
+
+            Vertex v = vertexes.get(i);
+
+            // Überprüfe: Sind wir auf diesem Feld?
+            if (v.getPosition().getX() == hero.getPos().getX() && v.getPosition().getY() == hero.getPos().getY()) {
+                me = v;
+            }
+
+            // Überprüfe: Felder worauf etwas steht werden nicht einbezogen.
+            if (v.getTileType().equals("##") || v.getTileType().equals("[]") || v.getTileType().startsWith("$")) {
+                continue;
+            }
+
+            for (int j = col - 1; j <= col + 1; j += 2) {
+                if (j >= 0 && j < board.getSize()) {
+                    Vertex adjacentV = vertexes.get(j * board.getSize() + row);
+                    v.getAdjacencies().add(adjacentV);
+                }
+            }
+
+            for (int j = row - 1; j <= row + 1; j += 2) {
+                if (j >= 0 && j < board.getSize()) {
+                    Vertex adjacentV = vertexes.get(col * board.getSize() + j);
+                    v.getAdjacencies().add(adjacentV);
+                }
+            }
+        }
+
+        PriorityQueue<Vertex> vertexQueue = new PriorityQueue<Vertex>();
+        vertexQueue.add(me);
+        me.setMinDistance(0);
+
+        while (!vertexQueue.isEmpty()) {
+            Vertex v = vertexQueue.poll();
+            double distance = v.getMinDistance() + 1;
+
+            for (Vertex neighbor : v.getAdjacencies()) {
+                if (distance < neighbor.getMinDistance()) {
+                    neighbor.setMinDistance(distance);
+                    neighbor.setPrevious(v);
+                    vertexQueue.remove(neighbor);
+                    vertexQueue.add(neighbor);
+                }
+            }
+        }
+
+        return vertexes;
+    }
+
+    private static List<Vertex> getPath(Vertex target) {
+        List<Vertex> path = new LinkedList<Vertex>();
+
+        path.add(target);
+        Vertex next = target;
+        while (next.getPrevious().getMinDistance() != 0) {
+            path.add(next.getPrevious());
+            next = next.getPrevious();
+        }
+
+        Collections.reverse(path);
+        return path;
+    }
+
+    @Override
+    public BotMove move(GameState gameState) {
+
+        //Pfade suchen nächste(r) (Schenke, Mine)
+        List<Vertex> vertexes = doDijkstra(gameState.getGame().getBoard(), gameState.getHero());
+
+        Vertex closestMine = null;
+        Vertex closestPub = null;
+
+        for (Vertex v : vertexes) {
+            if (v.getTileType().startsWith("$") && v.getMinDistance() != 0 && v.getMinDistance() != Double.POSITIVE_INFINITY && (closestMine == null || closestMine.getMinDistance() > v.getMinDistance())) {
+                if(!(v.getTileType().startsWith("$" + gameState.getHero().getId()))){
+                    closestMine = v;
+                }
+            }
+
+            if (v.getTileType().equals("[]") && v.getMinDistance() != Double.POSITIVE_INFINITY && (closestPub == null || closestPub.getMinDistance() > v.getMinDistance())) {
+                closestPub = v;
+            }
+        }
+
+        Vertex move = getPath(closestMine).get(0);
+
+        if (gameState.getHero().getGold() >= 2 && gameState.getHero().getLife() <= 39) {
+            move = getPath(closestPub).get(0);
+        } else {
+            move = getPath(closestMine).get(0);
+        }
+
+        //Spiel wurde beendet!
+        if (gameState.getGame().getTurn() >= 1196) {
+            logger.info("Spiel wurde beendet!");
+        }
+
+        return BotUtils.directionTowards(gameState.getHero().getPos(), move.getPosition());
+    }
+
+    @Override
+    public void setup() {
+        // No-op
+    }
+
+    @Override
+    public void shutdown() {
+        // No-op
+    }
+
+    private static class Vertex implements Comparable<Vertex> {
+        private String tileType;
+        private List<Vertex> adjacencies;
+        private double minDistance;
+        private Vertex previous;
+        private GameState.Position position;
+
+        private Vertex() {
+            this.minDistance = Double.POSITIVE_INFINITY;
+
+            this.adjacencies = new ArrayList<Vertex>(50 * 50);
+        }
+
+        public GameState.Position getPosition() {
+            return position;
+        }
+
+        public void setPosition(GameState.Position position) {
+            this.position = position;
+        }
+
+        public String getTileType() {
+            return tileType;
+        }
+
+        public void setTileType(String tileType) {
+            this.tileType = tileType;
+        }
+
+        public List<Vertex> getAdjacencies() {
+            return adjacencies;
+        }
+
+        public void setAdjacencies(List<Vertex> adjacencies) {
+            this.adjacencies = adjacencies;
+        }
+
+        public double getMinDistance() {
+            return minDistance;
+        }
+
+        public void setMinDistance(double minDistance) {
+            this.minDistance = minDistance;
+        }
+
+        public Vertex getPrevious() {
+            return previous;
+        }
+
+        public void setPrevious(Vertex previous) {
+            this.previous = previous;
+        }
+
+        @Override
+        public int compareTo(Vertex o) {
+            return Double.compare(getMinDistance(), o.getMinDistance());
+        }
+    }
+}
